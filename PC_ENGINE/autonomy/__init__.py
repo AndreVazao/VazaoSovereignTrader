f97cb6d0f8dee37d59080ebca763cbbf6d9c116d
André Vazão <vazao.andre@gmail.com>
2026-09-19 19:05:13 +0100

    feat(autonomy): add GOD Ultra package

---
+++ PC_ENGINE/autonomy/__init__.py
@@ -0,0 +1,5 @@
+"""Autonomous GOD Ultra decision and execution coordination layer."""
+
+from .god_ultra import GodUltraEngine, Opportunity, ExecutionIntent, RiskSnapshot
+
+__all__ = ["GodUltraEngine", "Opportunity", "ExecutionIntent", "RiskSnapshot"]
